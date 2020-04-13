a9cdeab105b3276a341f8872ef34d86a7fa6da6d
Bartosz Gertych <bartosz.gertych@tivo.com>
2020-04-13 13:01:13 +0200

    git init

---
+++ src/main/java/com/balaur/chamberlain/ChamberlainApplication.java
@@ -0,0 +1,13 @@
+package com.balaur.chamberlain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamberlainApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(ChamberlainApplication.class, args);
+  }
+}
---
+++ src/test/java/com/balaur/chamberlain/ChamberlainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balaur.chamberlain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamberlainApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
