b59a7af291e0f82e9f664104b404cbc63b3a3b80
Bartosz Gertych <bartosz.gertych@tivo.com>
2020-06-17 12:32:15 +0200

    fill database with examples

--- chamberlain-dao/src/main/resources/db/migration/V1__Create_tables.sql
+++ chamberlain-dao/src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,10 +1,18 @@
+CREATE TABLE product_type_table
+(
+    id                  BIGINT AUTO_INCREMENT           NOT NULL,
+    type                VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+    CONSTRAINT pk_product_type_table PRIMARY KEY (id)
+);
+
 CREATE TABLE product_table
 (
-    id                  BIGINT AUTO_INCREMENT   NOT NULL,
-    name                VARCHAR(255)            NOT NULL,
-    price               DOUBLE                  NOT NULL,
-    type                VARCHAR(255)            NOT NULL,
-    measureType         VARCHAR(255)            NOT NULL,
-    description         TEXT                    NOT NULL,
-    CONSTRAINT pk_product_table PRIMARY KEY (id)
+    id                  BIGINT AUTO_INCREMENT           NOT NULL,
+    name                VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+    type_id             BIGINT                          NOT NULL,
+    price               DOUBLE                          NOT NULL,
+    measureType         VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+    description         TEXT CHARACTER SET utf8         NOT NULL,
+    CONSTRAINT pk_product_table PRIMARY KEY (id),
+    FOREIGN KEY (type_id) REFERENCES product_type_table (id)
 );
